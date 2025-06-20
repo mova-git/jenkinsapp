56931083633a000afb0dfa53135b55256f95665e
mova-git <mauriya1999@gmail.com>
2025-06-20 22:20:09 +0530

    Update App.java

--- src/main/java/com/demo/App.java
+++ src/main/java/com/demo/App.java
@@ -1,1 +1,8 @@
-package com.demo; public class App { public static void main(String[] args) { System.out.println("Hello from GitHub project!"); }}
+package com.demo; 
+public class App 
+{ 
+	public static void main(String[] args) 
+	{ 
+		System.out.println("Hello from GitHub project!"); 
+	}
+}
